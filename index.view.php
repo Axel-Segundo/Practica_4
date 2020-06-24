6fa67b2178c73a92e28fb6c0388bc434fb983efb
Axel Segundo <axeljasegundo@gmail.com>
2020-06-24 14:39:43 -0500

    Creando estructura HTML

--- index.view.php
+++ index.view.php
@@ -4,9 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario contacto</title>
+    <link href="https://fonts.googleapis.com/css2?family=Piedra&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    
+    <div class="wrap">
+        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre:" value="">
+            <input type="email" class="form-control" id="correo" name="correo" placeholder="Correo:" value="">
+            <textarea name="mensaje" class="form-control" id="mensaje" placeholder="mensaje:"></textarea>
+            <input type="submit" name="submit" class="btn btn-primary" value="Enviar correo">
+        </form>
+    </div>
 </body>
 </html>
