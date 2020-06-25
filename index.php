53697a05952ce7ad19a0f5659594b4bbb9f0fd0f
Axel Segundo <axeljasegundo@gmail.com>
2020-06-24 20:05:32 -0500

    Finalizando validaciones

--- index.php
+++ index.php
@@ -18,12 +18,32 @@ if (isset($_POST['submit'])) {
     if (!empty($correo)) {
         $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
 
-        if(filter_var($correo, FILTER_VALIDATE_EMAIL)){
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
             $errores .= 'Por favor ingresa un correo valido <br />';
         }
     } else {
         $errores .= 'Por favor ingresa un correo <br />';
     }
+
+    if(!empty($mensaje)){
+        $mensaje = htmlspecialchars($mensaje);
+        $mensaje = trim($mensaje);
+        $mensaje = stripslashes($mensaje);
+    } else {
+        $errores .= 'Por favor ingresa el mensaje <br />';
+    }
+
+    if(!$errores){
+        $enviar_a = 'tunombre@tuempresa.com';
+        $asunto = 'Correo enviado desde miPagina.com';
+        $mensaje_preparado = "De: $nombre \n";
+        $mensaje_preparado .= "Correo: $correo \n";
+        $mensaje_preparado .= "Mensaje: " . $mensaje;
+
+        mail($enviar_a, $asunto, $mensaje_preparado);
+        $enviado = 'true';
+    }
+    
 }
 
 require 'index.view.php';
